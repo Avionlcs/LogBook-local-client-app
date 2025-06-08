4959c2d63bd98a47a0643eb5c39e0d9ddc76d618
harsha0771 <harshagihan00@gmail.com>
2025-06-08 20:59:29 +0530

    fix: Restore commented-out code in app.js for proper functionality |DC73|

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const path = require("path");
-// const app = express();
-// const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const cookieParser = require('cookie-parser');
 
-// const port = 90;
-// require('./src/utils/db.lock')
-// app.use(bodyParser.json({ limit: "100mb" }));
-// app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
-// app.use(cors({ origin: "*" }));
-// app.use(cookieParser());
-// app.use((req, res, next) => {
-//     if (req.method !== "GET") console.log(`${req.method} ${req.originalUrl}`);
-//     next();
-// });
-// const authMiddleware = require('./src/middleware/authentication.middleware');
-// app.use(authMiddleware.auth);
-// const creditMiddleware = require("./src/middleware/creditMiddleware");
-// app.use(creditMiddleware);
-// const logsMiddleware = require('./src/middleware/logs.middleware')
-// app.use(logsMiddleware)
-// const isPkg = typeof process.pkg !== "undefined";
-// const basePath = isPkg ? path.dirname(process.execPath) : __dirname;
-// const staticFilesPath = path.join(basePath, "out", "dist", "frontend", "browser");
-// app.use(express.static(staticFilesPath));
+const port = 90;
+require('./src/utils/db.lock')
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
+app.use(cors({ origin: "*" }));
+app.use(cookieParser());
+app.use((req, res, next) => {
+    if (req.method !== "GET") console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+const authMiddleware = require('./src/middleware/authentication.middleware');
+app.use(authMiddleware.auth);
+const creditMiddleware = require("./src/middleware/creditMiddleware");
+app.use(creditMiddleware);
+const logsMiddleware = require('./src/middleware/logs.middleware')
+app.use(logsMiddleware)
+const isPkg = typeof process.pkg !== "undefined";
+const basePath = isPkg ? path.dirname(process.execPath) : __dirname;
+const staticFilesPath = path.join(basePath, "out", "dist", "frontend", "browser");
+app.use(express.static(staticFilesPath));
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(staticFilesPath, "index.html"));
-// });
+app.get("/", (req, res) => {
+    res.sendFile(path.join(staticFilesPath, "index.html"));
+});
 
-// const dataRoutes = require("./src/routes/dataRoutes");
-// const authRoutes = require("./src/routes/authRoutes").router;
-// const utilityRoutes = require("./src/routes/utilityRoutes");
-// const printRoutes = require("./src/routes/printRoutes");
+const dataRoutes = require("./src/routes/dataRoutes");
+const authRoutes = require("./src/routes/authRoutes").router;
+const utilityRoutes = require("./src/routes/utilityRoutes");
+const printRoutes = require("./src/routes/printRoutes");
 
-// app.use("/", dataRoutes);
-// app.use("/", authRoutes);
-// app.use("/", utilityRoutes);
-// app.use("/", printRoutes);
+app.use("/", dataRoutes);
+app.use("/", authRoutes);
+app.use("/", utilityRoutes);
+app.use("/", printRoutes);
 
-// app.use((err, req, res, next) => {
-//     if (err instanceof require("multer").MulterError) {
-//         console.log(`Multer error: `, err);
-//         if (err.code === "LIMIT_FILE_SIZE") {
-//             return res.status(400).send({ error: "File size exceeds the limit of 200MB." });
-//         }
-//     }
-//     next(err);
-// });
+app.use((err, req, res, next) => {
+    if (err instanceof require("multer").MulterError) {
+        console.log(`Multer error: `, err);
+        if (err.code === "LIMIT_FILE_SIZE") {
+            return res.status(400).send({ error: "File size exceeds the limit of 200MB." });
+        }
+    }
+    next(err);
+});
 
-// const { getNetworkInterfaces } = require("./src/utils/networkUtils");
+const { getNetworkInterfaces } = require("./src/utils/networkUtils");
 
 app.get("/network-interfaces", (req, res) => {
     const addresses = getNetworkInterfaces(port);
