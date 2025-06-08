d039e7880e1a3c41670d44a3d47457fbe8c33e4e
harsha0771 <harshagihan00@gmail.com>
2025-06-08 12:17:28 +0530

    fix: Update target value calculation to use base64 decoded deploy script |SWE|

--- calculate-target.js
+++ calculate-target.js
@@ -37,12 +37,3 @@ const timeSegment = Math.floor(Date.now() / 200000);
 const input = timeSegment + deployKey;
 const hash2 = crypto.createHash('sha256').update(input.toString() + digest).digest('hex').slice(0, 4).toLocaleUpperCase();
 console.log(hash2)
-
-// node - e`
-//             const crypto = require('crypto');
-//             const deployKey = ${{ secrets.DEPLOY_KEY }};
-//             const timeSegment = Math.floor(Date.now() / 200000);
-//             const input = timeSegment + deployKey;
-//             const hash = crypto.createHash('sha256').update(input.toString()).digest('hex').slice(0, 4);
-//             console.log(hash);
-//           `
