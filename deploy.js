4a16226fbddbdf39b7d901c9c373b9366891c8d3
harsha0771 <harshagihan00@gmail.com>
2025-06-08 11:56:22 +0530

    fix: Refine commit message cleanup regex to correctly remove unwanted characters |7XF8| -test

--- deploy.js
+++ deploy.js
@@ -60,7 +60,7 @@ async function uploadToFirebase(buffer, destPath) {
             commitMessage = execSync('git log -1 --pretty=%B').toString().trim();
         } catch (err) { }
         if (commitMessage) {
-            commitMessage = commitMessage.replace(/[|a-zA-Z\d]+/g, '').trim();
+            commitMessage = commitMessage.replace(/\|[^|]+\|/g, '').trim();
             const prefixMatch = commitMessage.match(/^([^:]+:)\s*(.*)$/);
             if (prefixMatch) {
                 commitMessage = prefixMatch[2].trim();
