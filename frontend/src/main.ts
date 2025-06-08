576b5f84428d11fcf90288723d1cefa1aea3893e
harsha0771 <harshagihan00@gmail.com>
2025-06-08 19:03:45 +0530

    fix: Remove commented lines in main.ts for cleaner code |E62A|

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,20 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-// Function to set page opacity
+
 function setPageOpacity(opacity: string) {
   document.body.style.opacity = opacity;
 }
 
-// Add event listeners for focus and blur
 window.addEventListener('focus', () => {
-  setPageOpacity('1'); // Full opacity when focused
+  setPageOpacity('1');
 });
 
 window.addEventListener('blur', () => {
-  setPageOpacity('1'); // Reduced opacity when not focused
+  setPageOpacity('1');
 });
 
-// Bootstrap the Angular application
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
