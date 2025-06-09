6eb288dcaa822737e9f8c94c4728b0a9a0c6df42
harsha0771 <harshagihan00@gmail.com>
2025-06-09 17:18:11 +0530

    fix: Restore and configure TerserPlugin for code optimization and re-enable JavaScript obfuscation in webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -34,28 +34,38 @@ module.exports = {
     optimization: {
         minimize: true,
         minimizer: [
-
+            new TerserPlugin({
+                terserOptions: {
+                    compress: {
+                        drop_console: true, // Remove console statements
+                    },
+                    mangle: true, // Mangle variable names
+                    output: {
+                        comments: false, // Remove comments
+                    },
+                },
+            }),
         ],
     },
     plugins: [
-        // new JavaScriptObfuscator(
-        //     {
-        //         rotateUnicodeArray: true,
-        //         compact: true,
-        //         controlFlowFlattening: true,
-        //         controlFlowFlatteningThreshold: 0.9, // Higher threshold
-        //         deadCodeInjection: true,
-        //         deadCodeInjectionThreshold: 0.4,
-        //         debugProtection: true,
-        //         debugProtectionInterval: true,
-        //         disableConsoleOutput: true,
-        //         identifiersGenerator: "mangled",
-        //         selfDefending: true,
-        //         sourceMap: false,
-        //         stringArray: true, // Use string array
-        //         stringArrayThreshold: 0.75, // High threshold for string array
-        //     },
-        //     ["bundle.js"]
-        // ),
+        new JavaScriptObfuscator(
+            {
+                rotateUnicodeArray: true,
+                compact: true,
+                controlFlowFlattening: true,
+                controlFlowFlatteningThreshold: 0.9, // Higher threshold
+                deadCodeInjection: true,
+                deadCodeInjectionThreshold: 0.4,
+                debugProtection: true,
+                debugProtectionInterval: true,
+                disableConsoleOutput: true,
+                identifiersGenerator: "mangled",
+                selfDefending: true,
+                sourceMap: false,
+                stringArray: true, // Use string array
+                stringArrayThreshold: 0.75, // High threshold for string array
+            },
+            ["bundle.js"]
+        ),
     ],
 };
