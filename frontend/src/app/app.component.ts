0d67141186fa8b8fb26820fcf90ba932219b787e
harsha0771 <harshagihan00@gmail.com>
2025-06-10 22:25:52 +0530

    fix: Remove commented-out code and clean up unused imports in AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { HttpClient } from '@angular/common/http';
-import { AuthenticationService } from './pages/authentication/authentication.service';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { CommonModule } from '@angular/common';
 import { OrientationService } from './services/orientation.service';
@@ -23,20 +22,10 @@ export class AppComponent {
   constructor(private http: HttpClient, private router: Router, private orientationService: OrientationService) { }
 
   ngOnInit(): void {
-
-    // this.orientationService.orientationChanged.subscribe((isMobile: boolean) => {
-    //   this.isDesktop = !isMobile;
-    //   // Optionally lock to landscape if needed
-    //   // if (isMobile) this.orientationService.lockToLandscape();
-    // });
-
     if (this.orientationService.isMobileDevice()) {
-      //   this.isDesktop = false;
-      // this.orientationService.lockToLandscape();
     }
     this.router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        // Check if the current route is in the restricted routes list
         this.isRestrictedRoute = this.restrictedRoutes.includes(event.urlAfterRedirects);
       }
     });
@@ -57,19 +46,11 @@ export class AppComponent {
   }
 
   applyTheme(theme: any) {
-
     localStorage.setItem('theme', JSON.stringify(theme));
-
     if (!theme || typeof theme !== 'object') return;
-
-    // Store the theme in local storage
-
-    // Ensure theme.styles and theme.styles.variables exist
     if (theme.styles?.variables && typeof theme.styles.variables === 'object') {
-      // Replace CSS variables with values from the theme object
       Object.entries(theme.styles.variables).forEach(([key, value]) => {
         if (typeof value === 'string') {
-          // Replace underscores in key names with hyphens
           document.documentElement.style.setProperty(`--${key.replace(/_/g, '-')}`, value);
         }
       });
