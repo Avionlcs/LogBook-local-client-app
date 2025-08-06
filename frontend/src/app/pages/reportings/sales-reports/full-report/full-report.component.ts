550de2cfdea43551be52ae51f3f270304988fb53
harsha0771 <harshagihan00@gmail.com>
2025-08-06 13:19:54 +0530

    Refactor FullReport component: update user selection to seller, add report type selection, and implement API call to fetch sellers. Enhance styles for input groups and labels.

--- frontend/src/app/pages/reportings/sales-reports/full-report/full-report.component.ts
+++ frontend/src/app/pages/reportings/sales-reports/full-report/full-report.component.ts
@@ -1,17 +1,37 @@
 import { Component } from '@angular/core';
 import { ModalPopupComponent } from '../../../../components/modal-popup/modal-popup.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-full-report',
   standalone: true,
-  imports: [ModalPopupComponent],
+  imports: [ModalPopupComponent, CommonModule, FormsModule],
   templateUrl: './full-report.component.html',
   styleUrl: './full-report.component.scss'
 })
 export class FullReportComponent {
   modalVisible2A3: any = { hash: '', value: false };
+  selectedSellar: any = null;
+  sellars: any[] = [];
+  reportType: string = "daily";
+
+  ngOnInit() {
+    this.getAllSellars();
+  }
 
   changeModalVisible(vl: boolean) {
     this.modalVisible2A3 = { hash: Date.now(), value: vl }
+  }
+
+  getAllSellars() {
+    fetch('/api/reportings/sales/get-all-sellars')
+      .then(response => response.json())
+      .then(data => {
+        this.sellars = data;
+      })
+      .catch(error => {
+        console.error('Error fetching sellars:', error);
+      });
   }
 }
