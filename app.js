3638753835cb93316f0c85ce6d5c4dde68267996
harsha0771 <harshagihan00@gmail.com>
2025-08-19 10:27:13 +0530

    Revert "sss"
    This reverts commit 80b29949f70bbc2daab1697bd7c4aabea7ebe4a6.

--- app.js
+++ app.js
@@ -5,12 +5,16 @@ if (cluster.isMaster) {
     const numCPUs = os.cpus().length;
     cluster.schedulingPolicy = cluster.SCHED_RR;
 
+    // Fork workers
     for (let i = 0; i < numCPUs; i++) {
         cluster.fork();
     }
+    // Restart worker if it dies
     cluster.on('exit', (worker, code, signal) => {
+        // ////console.log(`Worker ${worker.process.pid} died. Restarting...`);
         cluster.fork();
     });
 } else {
-    require('./src/server.js');
+    // Worker: run your server code
+    require('./src/server.js'); // this is your code (file with express setup)
 }
