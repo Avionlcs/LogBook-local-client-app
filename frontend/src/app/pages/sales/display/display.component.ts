077aee8be74365b323ce66040424d78ea938cc0f
harsha0771 <harshagihan00@gmail.com>
2025-09-20 10:21:59 +0530

    ✨ Enable switching between inventory and sales display modes
    This change introduces a new feature that allows users to toggle between displaying inventory items and sales items within the display component. I think this is a good first step, but the implementation feels a bit clunky with the string comparisons and could benefit from a more robust state management solution in the future.

--- frontend/src/app/pages/sales/display/display.component.ts
+++ frontend/src/app/pages/sales/display/display.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SalesItemsListComponent } from './sales-items-list/sales-items-list.component';
+import { InventoryItemsListComponent } from './inventory-items-list/inventory-items-list.component';
 
 @Component({
   selector: 'app-display',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, InventoryItemsListComponent, SalesItemsListComponent],
   templateUrl: './display.component.html',
   styleUrl: './display.component.scss'
 })
 export class DisplayComponent {
+  mode: string = 'inventory';
 
+  changeMode() {
+    this.mode = this.mode == 'inventory' ? 'sales' : 'inventory';
+  }
 }
