59d868ba5fdc52e4007b0cfec8442b9fab38d176
harsha0771 <harshagihan00@gmail.com>
2025-09-21 14:36:09 +0530

    Refactor sales counter to use components ♻️
    This change was necessary to improve the modularity and
    maintainability of the sales counter feature. The original
    implementation was tightly coupled and difficult to extend.
    By breaking it down into smaller, reusable components, we
    can simplify the codebase and make it easier to add new
    features in the future.

--- frontend/src/app/pages/sales/counter/counter.component.ts
+++ frontend/src/app/pages/sales/counter/counter.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TimeAgoPipe } from '../display/inventory-items-list/inventory-item/time-ago.pipe';
+import { SelectCustomerComponent } from './select-customer/select-customer.component';
+import { ReceiptItemsComponent } from './receipt-items/receipt-items.component';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [SelectCustomerComponent, ReceiptItemsComponent, CounterComponent, TimeAgoPipe],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
+  @Input() sale: any = {};
+  @Output() saleProcess = new EventEmitter<any>();
 
+  onSelectCustomer(customer: any) {
+    this.sale.customer = customer;
+    this.saleProcess.emit(this.sale);
+  }
+
+  onChangeItems(items: any) {
+    this.sale.items = items;
+    this.saleProcess.emit(this.sale);
+  }
+
+  onUpdateSale(sale: any) {
+    this.sale = sale;
+    this.saleProcess.emit(this.sale);
+  }
 }
