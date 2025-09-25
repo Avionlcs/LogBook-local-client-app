3033ff07906aa298610c0e88a5db0aa4c8e9b2d5
harsha0771 <harshagihan00@gmail.com>
2025-09-26 00:38:41 +0530

    Revert "Add print receipt endpoint for sales 🖨️"
    This reverts commit 25a90c12528540a229db79463ccebf1ed82d2a9e.

--- src/api/api.routes.js
+++ src/api/api.routes.js
@@ -22,7 +22,6 @@ const { getSaleByPublicId } = require("./sales/get/getSaleByPublicId.api");
 const { updateItemQuantityInSale } = require("./sales/edit/updateItemQuantityInSale.api");
 const { verifySale } = require("./sales/helpers/verifySale.api");
 const completeSaleController = require("./sales/complete/completeSale.controller");
-const printReceiptApi = require("./sales/print/printReceipt.api");
 
 // New sales endpoints
 // const { initiateSale } = require("./sales/initiateSale.api");
@@ -65,11 +64,6 @@ router.post("/sales/payment", salesPermissions, paymentSale);
 router.get("/sales", salesPermissions, getSaleApi);
 router.post("/sales/complete", salesPermissions, completeSaleController);
 router.get("/sales/:sale_public_id", salesPermissions, getSaleByPublicId);
-router.post(
-    "/sales/:sale_public_id/print-receipt",
-    permissionMiddleware("sales"),
-    printReceiptApi
-);
 
 
 module.exports = router;
